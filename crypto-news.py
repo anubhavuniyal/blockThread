8a41123dd11ac1e5bbea62b748cf5758cc9a0382
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 <anubhavuniyal0917@protonmail.com>
2021-04-25 19:53:44 +0530

    Added ability to track multiple currencies
    - Track price of multiple currencies together, using a space seperated
    input
    - Ability to specify the duration of data to fetch, for e.g inputting 10 will
    fetch the data for the past 10 days

--- crypto-news.py
+++ crypto-news.py
@@ -10,19 +10,21 @@ from datetime import timedelta
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-d","--data",choices=["feeds","influencers","influencer","price-ticker"],help="Choose the type of data you want to see",default="price-ticker")
+group.add_argument("-d","--data",choices=["feeds","price-ticker"],help="Choose the type of data you want to see",default="price-ticker")
+parser.add_argument("--days",type=int,help="Number of days worth of data to show",default=30)
 group.add_argument("--coinoftheday",help="Display information about the current coin of the day",action="store_true")
 parser.add_argument("-k","--key",help="Your lunar api key")
-parser.add_argument("-s","--symbol",help="Details of a coin to display",default="BTC")
+parser.add_argument("-s","--symbol",nargs='+',help="Details of a coin to display",default=['BTC'])
 parser.add_argument("-l","--limit",help="Number of data to show",default=10)
 parser.add_argument("--sources",choices=["twitter","reddit"],help="Sources to get the information from",default="twitter")
 args = parser.parse_args()
 
 data = args.data
 key = args.key
-symbol = args.symbol
+symbols = args.symbol
 limit = args.limit
 sources=args.sources
+days = args.days
 
 if args.coinoftheday:
     parameters = {"data":"coinoftheday","key":key}
@@ -37,45 +39,48 @@ if args.coinoftheday:
     sys.exit(0)
 
 if data == "price-ticker":
+    number_of_arguments = len(symbols)
     end = date.today()
-    start = end - timedelta(days=30)
-    r = requests.get(f"https://data.messari.io/api/v1/assets/{symbol}/metrics/price/time-series?start={start}&end={end}&interval=1d")
-    json_response = r.json()
+    start = end - timedelta(days=days)
+    figure, axis = plt.subplots(number_of_arguments)
+    p1 = 0
+    for symbol in symbols:
+        r = requests.get(f"https://data.messari.io/api/v1/assets/{symbol}/metrics/price/time-series?start={start}&end={end}&interval=1d")
+        json_response = r.json()
 
 
-    for data in json_response.keys():
-        if data == "data":
-            value = list(json_response.values())
-            dict_value = value[1]
-            coin_value = dict_value['values']
+        for data in json_response.keys():
+            if data == "data":
+                value = list(json_response.values())
+                coin_value = value[1]['values']
 
-    x = []
-    o_y = []
-    h_y = []
-    l_y = []
-    c_y = []
-    day = 0
+        x = []
+        o_y = []
+        h_y = []
+        l_y = []
+        c_y = []
+        day = 0
 
-    for i in coin_value:
-        o_y.append(i[1])
-        h_y.append(i[2])
-        l_y.append(i[3])
-        c_y.append(i[4])
-        x.append(day)
-        day+=1
+        for i in coin_value:
+            o_y.append(i[1])
+            h_y.append(i[2])
+            l_y.append(i[3])
+            c_y.append(i[4])
+            x.append(day)
+            day+=1
 
-    plt.plot(x,o_y,label="Opening value")
-    plt.plot(x,h_y,label="Highest value")
-    plt.plot(x,l_y,label="Lowest value")
-    plt.plot(x,c_y,label="Closing value")
-    plt.legend()
-    plt.xlabel(f"From {start} to {end}")
-    plt.ylabel("Price changes")
-    plt.title(f"{symbol} price ticker")
+        axis[p1].plot(x,o_y,label="Opening value")
+        axis[p1].plot(x,h_y,label="Highest value")
+        axis[p1].plot(x,l_y,label="Lowest value")
+        axis[p1].plot(x,c_y,label="Closing value")
+        axis[p1].legend()
+        axis[p1].set_title(f"{symbol} price ticker")
+        p1+=1
+    
     plt.show()
     sys.exit(0)
  
-
+symbol = symbols[0]
 parameters = {"data":data,"key":key,"symbol":symbol,"limit":limit,"sources":sources}
 
 r = requests.get("https://api.lunarcrush.com/v2", params=parameters)
